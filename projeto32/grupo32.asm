9d0354dfa37153e450bee2d147593df83c97798b
Irell Zane <itsirellzane@gmail.com>
2023-05-19 18:53:00 +0100

    adiciona varrimento das quatro linhas
    Em vez de verificar as teclas de apenas uma linha, o programa agora varre continuamente, em ciclo, as várias linhas do teclado.

--- projeto32/grupo32.asm
+++ projeto32/grupo32.asm
@@ -11,7 +11,8 @@
 DISPLAYS   EQU 0A000H  ; endereço dos displays de 7 segmentos (periférico POUT-1)
 TEC_LIN    EQU 0C000H  ; endereço das linhas do teclado (periférico POUT-2)
 TEC_COL    EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
-LINHA      EQU 8       ; linha a testar (4ª linha, 1000b)
+LINHA1      EQU 1       ; linha a testar (4ª linha, 1000b)
+LINHA4      EQU 8;
 MASCARA    EQU 0FH     ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
 
 ; **********************************************************************
@@ -29,21 +30,26 @@ inicio:
 ciclo:
     MOV  R1, 0 
     MOVB [R4], R1      ; escreve linha e coluna a zero nos displays
+    MOV  R1, LINHA1    ; testar a linha 4 
 
 espera_tecla:          ; neste ciclo espera-se até uma tecla ser premida
-    MOV  R1, LINHA     ; testar a linha 4 
     MOVB [R2], R1      ; escrever no periférico de saída (linhas)
     MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
     AND  R0, R5        ; elimina bits para além dos bits 0-3
     CMP  R0, 0         ; há tecla premida?
-    JZ   espera_tecla  ; se nenhuma tecla premida, repete
-                       ; vai mostrar a linha e a coluna da tecla
-    SHL  R1, 4         ; coloca linha no nibble high
-    OR   R1, R0        ; junta coluna (nibble low)
-    MOVB [R4], R1      ; escreve linha e coluna nos displays
+    JNZ  tecla_premida ; se houver uma tecla premida, salta
+    CMP R1, 5         ; chegou à última linha?
+    JGE ciclo           ; se chegou à última linha, repete ciclo
+    SHL R1, 1          ; testar a próxima linha
+    JMP espera_tecla   ; continua a testar a proxima linha
+    
+tecla_premida:         ; vai mostrar a linha e a coluna da tecla no display
+    MOV  R6, R1        ; copia linha
+    SHL  R6, 4         ; coloca linha no nibble high
+    OR   R6, R0        ; junta coluna (nibble low)
+    MOVB [R4], R6      ; escreve linha e coluna nos displays
     
 ha_tecla:              ; neste ciclo espera-se até NENHUMA tecla estar premida
-    MOV  R1, LINHA     ; testar a linha 4  (R1 tinha sido alterado)
     MOVB [R2], R1      ; escrever no periférico de saída (linhas)
     MOVB R0, [R3]      ; ler do periférico de entrada (colunas)
     AND  R0, R5        ; elimina bits para além dos bits 0-3

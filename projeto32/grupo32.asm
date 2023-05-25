cba79f64902697bc0bc60379d9de10d23651def2
Irell Zane <irellzanedev@gmail.com>
2023-05-25 10:32:40 +0100

    melhorar indentação

--- projeto32/grupo32.asm
+++ projeto32/grupo32.asm
@@ -8,47 +8,47 @@
 ; **********************************************************************
 ; * Constantes
 ; **********************************************************************
-DISPLAYS            EQU 0A000H  ; endereço dos displays de 7 segmentos (periférico POUT-1)
-TEC_LIN             EQU 0C000H  ; endereço das linhas do teclado (periférico POUT-2)
-TEC_COL             EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
-LINHA1              EQU 1       ; linha a testar (4ª linha, 1000b)
-LINHA4              EQU 8;
-MASCARA             EQU 0FH     ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
-TECLA_ESQUERDA		EQU 1		; tecla na primeira coluna do teclado (tecla C)
-TECLA_DIREITA		EQU 2		; tecla na segunda coluna do teclado (tecla D)
+DISPLAYS            EQU 0A000H   ; endereço dos displays de 7 segmentos (periférico POUT-1)
+TEC_LIN             EQU 0C000H   ; endereço das linhas do teclado (periférico POUT-2)
+TEC_COL             EQU 0E000H   ; endereço das colunas do teclado (periférico PIN)
+LINHA1              EQU 1        ; linha a testar (4ª linha, 1000b)
+LINHA4              EQU 8
+MASCARA             EQU 0FH      ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+TECLA_ESQUERDA      EQU 1        ; tecla na primeira coluna do teclado (tecla C)
+TECLA_DIREITA       EQU 2        ; tecla na segunda coluna do teclado (tecla D)
 
-COMANDOS				EQU	6000H			; endereço de base dos comandos do MediaCenter
+COMANDOS            EQU 6000H   ; endereço de base dos comandos do MediaCenter
 
-DEFINE_LINHA    		    EQU COMANDOS + 0AH		; endereço do comando para definir a linha
-DEFINE_COLUNA   		    EQU COMANDOS + 0CH		; endereço do comando para definir a coluna
-DEFINE_PIXEL    		    EQU COMANDOS + 12H		; endereço do comando para escrever um pixel
-APAGA_AVISO     		    EQU COMANDOS + 40H		; endereço do comando para apagar o aviso de nenhum cenário selecionado
-APAGA_ECRÃ	 		        EQU COMANDOS + 02H		; endereço do comando para apagar todos os pixels já desenhados
-SELECIONA_CENARIO_FUNDO     EQU COMANDOS + 42H		; endereço do comando para selecionar uma imagem de fundo
-TOCA_SOM				    EQU COMANDOS + 5AH		; endereço do comando para tocar um som
+DEFINE_LINHA        EQU COMANDOS + 0AH    ; endereço do comando para definir a linha
+DEFINE_COLUNA       EQU COMANDOS + 0CH    ; endereço do comando para definir a coluna
+DEFINE_PIXEL        EQU COMANDOS + 12H    ; endereço do comando para escrever um pixel
+APAGA_AVISO         EQU COMANDOS + 40H    ; endereço do comando para apagar o aviso de nenhum cenário selecionado
+APAGA_ECRÃ          EQU COMANDOS + 02H    ; endereço do comando para apagar todos os pixels já desenhados
+SELECIONA_CENARIO_FUNDO     EQU COMANDOS + 42H    ; endereço do comando para selecionar uma imagem de fundo
+TOCA_SOM            EQU COMANDOS + 5AH    ; endereço do comando para tocar um som
 
-LINHA_TOPO        	EQU 0       ; linha do topo do ecrã
-COLUNA_ESQ			EQU 0       ; coluna mais à esquerda
-COLUNA_CENT         EQU 32      ; coluna central
-COLUNA_DIR          EQU 63      ; coluna mais à direita
+LINHA_TOPO          EQU 0        ; linha do topo do ecrã
+COLUNA_ESQ          EQU 0        ; coluna mais à esquerda
+COLUNA_CENT         EQU 32       ; coluna central
+COLUNA_DIR          EQU 63       ; coluna mais à direita
 
-LINHA_PAINEL        EQU 27      ; linha do painel da nave
-COLUNA_PAINEL       EQU 25      ; coluna do painel da nave
+LINHA_PAINEL        EQU 27       ; linha do painel da nave
+COLUNA_PAINEL       EQU 25       ; coluna do painel da nave
 
-LARGURA_AST			EQU	5		; largura do asteroide
-ALTURA_AST          EQU 5       ; altura do asteroide 
+LARGURA_AST         EQU 5        ; largura do asteroide
+ALTURA_AST          EQU 5        ; altura do asteroide
 
-LARGURA_PAINEL      EQU 15      ; largura do painel da nave
-ALTURA_PAINEL       EQU 5       ; altura do painel da nave
+LARGURA_PAINEL      EQU 15       ; largura do painel da nave
+ALTURA_PAINEL       EQU 5        ; altura do painel da nave
 
-LARGURA_SONDA		EQU 1		;largura das sondas
-ALTURA_SONDA		EQU 1		;altura das sondas
+LARGURA_SONDA       EQU 1        ; largura das sondas
+ALTURA_SONDA        EQU 1        ; altura das sondas
 
-PIXEL_VERM		    EQU	0FF00H	; pixel vermelho opaco
-PIXEL_VERM_TRANS    EQU	0FF00H	; pixel vermelho translucido
-PIXEL_VERD          EQU 0F0F0H  ; pixel verde opaco 
-PIXEL_VERD_TRANS    EQU 0F0F0H  ; pixel verde translucido 
-PIXEL_AZUL          EQU 0F0BFH  ; pixel azul opaco 
+PIXEL_VERM          EQU 0FF00H   ; pixel vermelho opaco
+PIXEL_VERM_TRANS    EQU 0FF00H   ; pixel vermelho translucido
+PIXEL_VERDE         EQU 0FF0000H ; pixel verde opaco
+PIXEL_VERDE_TRANS   EQU 0FF0000H ; pixel verde translucido
+PIXEL_AZUL          EQU 0FFH     ; pixel azul opaco
 PIXEL_VIOLETA       EQU 0FA3CH  ; pixel violeta opaco 
 PIXEL_AMAR          EQU 0FFF0H  ; pixel opaco amarelo
 PIXEL_CAST          EQU 0F850H  ; pixel opaco castanho
